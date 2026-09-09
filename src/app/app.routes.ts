361f50891dba5c5c8b53c8993a732715f3a2a9f2
Luis Soares <luissoaress.dev@gmail.com>
2026-09-08 21:00:03 -0300

    feat: atualizar favicon, titles das paginas e meta tag description

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ export const routes: Routes = [
   {
     path: '',
     loadComponent: () => import('./features/auth/login/login').then((m) => m.Login),
+    title: 'Login',
   },
 
   {
     path: 'cadastro',
     loadComponent: () => import('./features/auth/cadastro/cadastro').then((m) => m.Cadastro),
+    title: 'Cadastro',
   },
 
   {
@@ -18,25 +20,30 @@ export const routes: Routes = [
       {
         path: 'home',
         loadComponent: () => import('./features/dash-home/dash-home').then((m) => m.DashHome),
+        title: 'Pagina Inicial',
       },
       {
         path: 'perfil',
         loadComponent: () => import('./features/perfil/perfil').then((m) => m.Perfil),
+        title: 'Perfil',
       },
       {
         path: 'agendamentos',
         loadComponent: () =>
           import('./features/agendamentos/agendamentos').then((m) => m.Agendamentos),
+        title: 'Agendamentos',
       },
       {
         path: 'buscar-servicos',
         loadComponent: () =>
           import('./features/buscar-servicos/buscar-servicos').then((m) => m.BuscarServicos),
+        title: 'Buscar Serviços',
       },
       {
         path: 'criar-servico',
         loadComponent: () =>
           import('./features/criar-servico/criar-servico').then((m) => m.CriarServico),
+        title: 'Criar Serviços',
       },
     ],
   },
